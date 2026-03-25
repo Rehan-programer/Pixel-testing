7dfa5c4138c66c9a6cd7e21316f2e08fb2e6773c
Rehan <rhnmhr786@gmail.com>
2026-03-25 18:53:18 +0500

    html size

--- src/app/page.js
+++ src/app/page.js
@@ -41,7 +41,7 @@ export default async function Home() {
       </div>
       {/* <OurServices data={homePageData?.OurServices} lang={lang} mainServices={mainServices} subServices={subServices}/> */}
       {/* <WhatAreTheBenefits data={homePageData.Benefits} lang={lang} /> */}
-      {/* <WhyNeed data={homePageData.WhyNeed} /> */}
+      <WhyNeed data={homePageData.WhyNeed} />
       {/* <Industries lang={lang}  mainServices={mainServices} subServices={subServices}/> */}
       <Vision data={homePageData.visionData} />
       <ServicePricing lang={lang} data={homePageData.ServicePricingData} />
