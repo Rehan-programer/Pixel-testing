08bdeb6829915c23497380f93f19992efdbdd8fe
Rehan <rhnmhr786@gmail.com>
2026-03-25 19:08:32 +0500

    html size

--- src/app/page.js
+++ src/app/page.js
@@ -33,21 +33,21 @@ export default async function Home() {
   return (
     <>
         <SchemaInjector page={"mainhome"} />
-      {/* <HomePageBanner data={homePageData} heading="home banner" lang={lang} />
+      <HomePageBanner data={homePageData} heading="home banner" lang={lang} />
       <DashBoardPortal data={homePageData?.PortalData} />
       <div className="container-global p-[0]">
         <OurClients data={homePageData?.OurClients} />
         <Launching data={homePageData?.LaunchingData} />
-      </div> */}
-      <OurServices data={homePageData?.OurServices} lang={lang} mainServices={mainServices} subServices={subServices}/>
-      {/* <WhatAreTheBenefits data={homePageData.Benefits} lang={lang} />
-      <WhyNeed data={homePageData.WhyNeed} /> */}
+      </div>
+      {/* <OurServices data={homePageData?.OurServices} lang={lang} mainServices={mainServices} subServices={subServices}/> */}
+      <WhatAreTheBenefits data={homePageData.Benefits} lang={lang} />
+      <WhyNeed data={homePageData.WhyNeed} />
       {/* <Industries lang={lang}  mainServices={mainServices} subServices={subServices}/> */}
-      {/* <Vision data={homePageData.visionData} />
+      <Vision data={homePageData.visionData} />
       <ServicePricing lang={lang} data={homePageData.ServicePricingData} />
       <Testimonials lang={lang} />
       <ContactUsFormCard lang={lang} />
-      <ContactCards /> */}
+      <ContactCards />
 
     </>
   );
